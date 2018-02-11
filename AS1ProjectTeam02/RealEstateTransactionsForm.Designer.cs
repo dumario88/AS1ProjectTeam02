0141d94bb1b69ecd558a912990cccf05a1b25721
Osbjorn Gulbranson <osbjorngulbranson@gmail.com>
2018-02-11 11:41:34 -0800

    XML add feature

---
+++ AS1ProjectTeam02/RealEstateTransactionsForm.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace AS1ProjectTeam02
+{
+    partial class RealEstateTransactionsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // RealEstateTransactionsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(712, 334);
+            this.Name = "RealEstateTransactionsForm";
+            this.Text = "RealEstateTransactionsForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ AS1ProjectTeam02/RealEstateTransactionsForm.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+namespace AS1ProjectTeam02
+{
+    public partial class RealEstateTransactionsForm : Form
+    {
+        List<House> transactionsList;
+        public RealEstateTransactionsForm()
+        {
+            InitializeComponent();
+
+            OpenTheXML();
+        }
+
+        public void OpenTheXML()
+        {
+            OpenFileDialog openFileDialogXML = new OpenFileDialog();
+            StreamReader inputFile;
+
+            if (openFileDialogXML.ShowDialog() == DialogResult.OK)
+            {
+                inputFile = File.OpenText(openFileDialogXML.FileName);
+            }
+            else return; // failure!!
+
+            // Xml serializer
+            XmlSerializer listSerializer = new XmlSerializer(typeof(List<House>));
+            // reads file into list
+            transactionsList = listSerializer.Deserialize(inputFile) as List<House>;
+            inputFile.Close();
+            Console.WriteLine(transactionsList);
+
+        }
+    }
+    [Serializable]
+    public class House
+    {
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string HouseType { get; set; }
+        public int SurfaceArea { get; set; }
+        public int Price { get; set; }
+        public int Bedrooms { get; set; }
+        public int Bathrooms { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Address},{City},{HouseType},{SurfaceArea},{Price},{Bedrooms},{Bathrooms}";
+        }
+    }
+
+}
